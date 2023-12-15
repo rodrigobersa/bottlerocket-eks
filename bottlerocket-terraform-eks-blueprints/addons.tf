54da8c1e6cc4181e0a488ac64c288e2fa7158d7f
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2023-12-15 17:19:39 -0500

    Initial commit

---
+++ bottlerocket-terraform-eks-blueprints/addons.tf
@@ -0,0 +1,61 @@
+################################################################################
+# Data Sources
+################################################################################
+data "aws_ecrpublic_authorization_token" "token" {
+  provider = aws.virginia
+}
+
+################################################################################
+# EKS Blueprints Addons
+################################################################################
+module "eks_blueprints_addons" {
+  source  = "aws-ia/eks-blueprints-addons/aws"
+  version = "~> 1.12"
+
+  cluster_name      = module.eks.cluster_name
+  cluster_endpoint  = module.eks.cluster_endpoint
+  cluster_version   = module.eks.cluster_version
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  enable_cert_manager = true
+
+  helm_releases = {
+    burpop-crd = {
+      description   = "CRDs for Bottlerocket Update Operator"
+      chart         = "bottlerocket-shadow"
+      chart_version = "1.0.0"
+      repository    = "https://bottlerocket-os.github.io/bottlerocket-update-operator/"
+    }
+    brupop-operator = {
+      description      = "A Helm chart for Bottlerocket Update Operator"
+      chart            = "bottlerocket-update-operator"
+      chart_version    = "1.3.0"
+      namespace        = "brupop-bottlerocket-aws"
+      create_namespace = true
+      repository       = "https://bottlerocket-os.github.io/bottlerocket-update-operator/"
+    }
+  }
+
+  enable_karpenter = true
+  karpenter = {
+    repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+    repository_password = data.aws_ecrpublic_authorization_token.token.password
+    version             = "v0.33"
+  }
+
+  tags = local.tags
+}
+
+################################################################################
+# Example application and dependencies Helm Chart
+################################################################################
+resource "helm_release" "application" {
+  name  = "application"
+  chart = "./application"
+  set_list {
+    name  = "nodepool.zone"
+    value = local.azs
+  }
+
+  depends_on = [module.eks_blueprints_addons]
+}
---
+++ bottlerocket-terraform-eks-blueprints/main.tf
@@ -0,0 +1,54 @@
+################################################################################
+# Providers
+################################################################################
+provider "aws" {
+  region = local.region
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "virginia"
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", local.region]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", local.region]
+    }
+  }
+}
+
+################################################################################
+# Data sources
+################################################################################
+data "aws_availability_zones" "available" {}
+
+data "aws_caller_identity" "current" {}
+
+################################################################################
+# Variables
+################################################################################
+locals {
+  name   = substr(basename(path.cwd), 1, 12)
+  region = "us-west-2"
+  tags = {
+    Sample     = local.name
+    GithubRepo = "github.com/aws-samples/amazon-eks-bottlerocket-mngnodegrp-terraform"
+  }
+}
